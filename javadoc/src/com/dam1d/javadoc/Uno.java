3ec33292dcf643db5c21ae2771af46c13cf35b0d
Sergio <smr.saznar.1950@iespabloserrano.com>
2021-04-15 10:08:27 +0200

    Confirmado desde Eclipse

---
+++ javadoc/src/com/dam1d/javadoc/Uno.java
@@ -0,0 +1,19 @@
+package com.dam1d.javadoc;
+
+/**
+ * Sirve para aprender javadoc
+ * @author Pluscuolter
+ *
+ */
+
+public class Uno {
+
+	/**
+	 * Sirve para probar
+	 * @param entero edad del empleado
+	 * @return decuelve la propina del empleado
+	 */
+	int probatina(int entero) {
+		return 88;
+	}
+}
